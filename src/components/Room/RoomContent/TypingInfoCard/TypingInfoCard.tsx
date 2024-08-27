995ca9903b0bac77e96d59acd5e5ce0574f96e4c
utkuonursahin <sahinutkuonur@gmail.com>
2024-08-27 20:56:31 +0300

    fix: typing notification user list length fix

--- src/components/Room/RoomContent/TypingInfoCard/TypingInfoCard.tsx
+++ src/components/Room/RoomContent/TypingInfoCard/TypingInfoCard.tsx
@@ -9,7 +9,13 @@ const TypingInfoCard = ({ users }: TypingInfoCardProps) => {
     return (
         <div className={`flex flex-col w-fit gap-1 px-4 py-2 rounded-md 'bg-primary/10'}`}>
             <p className="text-xs flex justify-between items-center gap-4">
-                <span className="text-primary">{users.map((user) => user.username).join(', ')}</span>
+                <span className="text-primary">
+                    {users
+                        .slice(0, 4)
+                        .map((user) => user.username)
+                        .join(', ')
+                        .concat(` + ${users.length - 4} more `)}
+                </span>
             </p>
             <p className="text-secondary-foreground">Typing...</p>
         </div>
